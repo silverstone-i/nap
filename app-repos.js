a882c784ce1c291e4050ad3efa86c01a95c1782c
Ian Silverstone <131495749+silverstone-i@users.noreply.github.com>
2023-05-24 23:06:52 -0400

    Implement Server Skeleton

---
+++ app-repos.js
@@ -0,0 +1,7 @@
+// Object of all data models used by application
+
+const appRepos = {
+
+}
+
+module.exports = appRepos;
---
+++ app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// ./app.js
+/**
+ * Application entry point
+ */
+
+const express = require('express');
+const morgan = require('morgan');
+const config = require('config');
+const path = require('path');
+const { DB } = require('nap-db');
+require('dotenv').config();
+const repositories = require('./app-repos');
+
+// Create the express app
+const app = express();
+
+// Install intial middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded()); // Parse URL encoded bodies
+app.use(morgan('tiny')); // Minimsl logging to console TODO: write to log file
+
+// Read configuration and environment variables to run server and connect to the database
+process.env.NODE_ENV ? process.env.NODE_ENV : 'development'; // Default is development runtime environment
+const connection = config.get(`runtimeEnv.${process.env.NODE_ENV}`); // DB connection object
+const HOST = connection.host;
+const PORT = connection.server_port || 3000; // Use PORT 3000 if server_port is not defined
+
+// Connect to database
+const db = DB.init(connection, repositories);
+
+//Test the connection
+db.connect()
+    .then((obj) => {
+        console.log('Connected to Postgres database!');
+        obj.done(); // success, release connection;
+    })
+    .catch((error) => {
+        console.log('Error connecting to Postgres database:', error.message);
+    });
+
+// TODO: Add additional required middleware
+//  User authentication
+//  User authorization
+//  Data validation and sanitization
+
+// TODO: Application routes and catchall errors
+app.get('/', (req, res) => res.send('Welkom')); // TODO: Renove this route
+
+// Start express server
+app.listen(PORT, HOST, (err) => {
+    if (err) console.log('Error in server setup', err.message);
+    console.log(`NAP server listening on http://${HOST}:${PORT}`);
+});
