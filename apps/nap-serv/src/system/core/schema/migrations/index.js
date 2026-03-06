78a12060100f7a0e61581e8c58e4d065231d06ee
ian <ian@isilverstone.com>
2026-03-06 00:20:08 -0500

    refactor(nap-serv): remove policyCatalogAvailableFields migration

--- apps/nap-serv/src/system/core/schema/migrations/index.js
+++ apps/nap-serv/src/system/core/schema/migrations/index.js
@@ -9,6 +9,5 @@ import coreRbac from './202502110010_coreRbac.js';
 import coreEntities from './202502110011_coreEntities.js';
 import numberingSystem from './202502250012_numberingSystem.js';
 import policyCatalogValidStatuses from './202503050013_policyCatalogValidStatuses.js';
-import policyCatalogAvailableFields from './202503050014_policyCatalogAvailableFields.js';
 
-export default [coreRbac, coreEntities, numberingSystem, policyCatalogValidStatuses, policyCatalogAvailableFields];
+export default [coreRbac, coreEntities, numberingSystem, policyCatalogValidStatuses];
